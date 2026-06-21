85f069e1f4abcca663a98a9ca63415d8865c0922
jaemin-cho-04 <justin_cho@icloud.com>
2026-06-21 16:39:02 -0700

    have to start writing main code

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <.clang-format>
+#include <gtest/gtest.h>
+#include <linalg/mat4.hpp>
+#include <linalg/mat3.hpp>
+#include <linalg/vec3.hpp>
+#include <cmath>
+
+constexpr double PI = 3.14159265358979323846;
+
+using linalg::Mat4;
+using linalg::Mat3;
+using linalg::Vec3;
+
